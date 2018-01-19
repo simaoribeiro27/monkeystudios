5a78d96613b3e4f07593a8a4c0ccad60059e38e2
simaoribeiro27 <simaoo_arp@hotmail.com>
2018-01-19 13:11:30 +0000

    trabalho prático

---
+++ routes/chart.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var Travel = require('../models/travel');
+var Reservation = require('../models/reservation');
+
+/* GET ALL TRAVELS and reservations*/
+router.get('/chart', function (req, res, next) {
+    Travel.find(function (err, travels) {
+        Reservation.find(function (err, reservations) {
+      if (err) { res.send(err); }
+      res.render('chart.ejs', {  travels, reservations});
+     });
+    });
+  });
+
+
+module.exports = router;
